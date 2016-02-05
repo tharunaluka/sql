04e74d165ce4f729c304de813cfabc4221a1b750
Tharun Aluka <tharunaluka@Tharuns-MacbooK-Pro.local>
2016-02-05 19:23:11 +0530

    first

--- app/src/main/java/com/acetechapps/sql/MainActivity.java
+++ app/src/main/java/com/acetechapps/sql/MainActivity.java
@@ -2,12 +2,37 @@ package com.acetechapps.sql;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String getQuery = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void save (View view) {
+
+    }
+
+    public void clear (View view) {
+
+    }
+
+    public void run (View view) {
+
+    }
+
+    public void inPut (View view) {
+
+        EditText writtenQuery = (EditText)findViewById(R.id.inPut);
+        getQuery = writtenQuery.getText().toString();
+
+    }
+
 }
---
+++ app/src/main/java/com/acetechapps/sql/OutputActivity.java
@@ -0,0 +1,15 @@
+package com.acetechapps.sql;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.TextView;
+
+
+/**
+ * Created by tharunaluka on 05/02/16.
+ */
+public class OutputActivity {
+
+}
